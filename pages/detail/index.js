628129a548868afd630dcfb353f438f0b2972013
liuyousama <634308664@qq.com>
2019-11-13 23:38:32 +0800

    深夜更新

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -5,18 +5,26 @@ const app = getApp()
 Page({
   data: {
     article:{},
-    bgList:["positive","negative","silent","active","angry"]
+    bgList:["positive","negative","silent","exciting","angry"]
   },
   onLoad: function (options) {
     var postid = options.postid
+    console.log(postid)
+    console.log(app.globalData.SERVER_URL+"post/post?id="+postid)
     var source = this
     qq.request({
-      url:"post/post?id="+postid,
+      url:app.globalData.SERVER_URL+"post/post?id="+postid,
       method:"GET",
-      success:this.res,
+      success:this.handleRes,
+      fail(res){
+        console.log(res)
+        qq.showToast({title:"网络异常",icon:"none"})
+      }
     })
   },
+  
   handleRes(res){
+    console.log(res)
     if (!res.data || res.data.code != 0) {
       qq.showToast({title:"网络异常",icon:"none"})
       qq.navigateBack(1)
@@ -36,5 +44,15 @@ Page({
     article.title = data.title
     article.bincat = data.bincat
     article.img = data.img
+    article.bg = 0
+    for (let key in this.data.bgList) {
+      if (this.data.bgList[key]==article.bincat) {
+        article.bg = key
+      }
+    }
+    
+    this.setData({
+      article:article
+    })
   },
 })
--- pages/detail/index.qml
+++ pages/detail/index.qml
@@ -3,7 +3,7 @@
   <view class="like">加入文集</view>
 </view>
 
-<view class="text-container {{bgList[article.type-1]}}">
+<view class="text-container {{bgList[article.bg]}}">
   <image src="{{article.img}}" mode="aspectFill" />
   <view qq:if="{{article.type == 2}}" class="article">{{article.content}}</view>
   <view class="short" qq:if="{{article.type == 1}}" qq:key="*this">
